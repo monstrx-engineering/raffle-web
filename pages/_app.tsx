ac7c62e20d6fab5e4845c557419a7c525c517dbc
Geraldi Sutanto <5717479+justomat@users.noreply.github.com>
2022-12-22 22:37:31 +0700

    update favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -48,7 +48,7 @@ export default function App(props: AppProps) {
 			<Head>
 				<title>Mantine next example</title>
 				<meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-				<link rel="shortcut icon" href="/favicon.svg" />
+				<link rel="shortcut icon" href="/favicon.ico" />
 			</Head>
 
 			<ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
