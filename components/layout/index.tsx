19aded9925d02602d7fd19eb04184028d4751e99
Geraldi Sutanto <5717479+justomat@users.noreply.github.com>
2023-01-04 15:50:22 +0700

    change background

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,16 +1,16 @@
-import { AppShell, useMantineTheme } from '@mantine/core';
+import { AppShell } from '@mantine/core';
 import type { ReactNode } from 'react';
 
 import { DefaultHeader } from './header';
 
 export function Layout({ children }: { children: ReactNode }) {
-	const theme = useMantineTheme();
-
 	return (
 		<AppShell
 			styles={{
 				main: {
-					background: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
+					backgroundImage: 'url("/bg.jpg")',
+					backgroundSize: 'cover',
+					backgroundPositionX: '87%',
 				},
 			}}
 			header={<DefaultHeader />}
