a70d476f48299a67e644064bfa1f483333e0ed51
Geraldi Sutanto <5717479+justomat@users.noreply.github.com>
2023-04-03 14:12:47 +0700

    fix eslint ts

--- functions/api/verify.ts
+++ functions/api/verify.ts
@@ -1,17 +1,16 @@
-import type {
-	JsonWebKey,
-	PagesFunction,
-	SubtleCryptoImportKeyAlgorithm,
-} from '@cloudflare/workers-types/2022-11-30';
 import jwt from '@tsndr/cloudflare-worker-jwt';
+import type {
+	PagesFunction,
+	SubtleCryptoImportKeyAlgorithm as Algorithm,
+} from '@cloudflare/workers-types/2022-11-30';
 
 type Request = {
-	signature: ArrayBuffer | ArrayBufferView;
-	message: ArrayBuffer | ArrayBufferView;
-	publicKey: (ArrayBuffer | ArrayBufferView) | JsonWebKey;
+	signature: Parameters<typeof crypto.subtle.verify>[2];
+	message: Parameters<typeof crypto.subtle.verify>[3];
+	publicKey: Parameters<typeof crypto.subtle.importKey>[1];
 };
 
-const REQUEST_ENCRYPTION: SubtleCryptoImportKeyAlgorithm = {
+const REQUEST_ENCRYPTION: Algorithm = {
 	name: 'NODE-ED25519',
 	namedCurve: 'NODE-ED25519',
 };
@@ -40,45 +39,46 @@ const Validator = {
 		}).then((r) => r.json()),
 };
 
-export const onRequest: PagesFunction = async (context) => {
-	let bytes = await context.request.arrayBuffer();
-	let [signature, publicKey, message] = partition(bytes, [64, 32, undefined]);
-	let ok = await verify({ signature, publicKey, message });
+export const onRequest: PagesFunction<{ JWT_SECRET: string }> = async (context) => {
+	try {
+		let bytes = await context.request.arrayBuffer();
+		let [signature, publicKey, message] = partition(bytes, [64, 32, undefined]);
+		let ok = await verify({ signature, publicKey, message });
 
-	if (!ok) return new Response(null, { status: 401 });
+		if (!ok) throw Error();
 
-	let claims = JSON.parse(new TextDecoder().decode(message));
+		let claims = JSON.parse(new TextDecoder().decode(message)) as Record<string, string>;
 
-	let validated = { wallet: claims.wallet };
-	if (claims.discord) {
-		validated.discord = await Validator.discord(claims.discord)
-			.then(({ user }) => `${user.username}#${user.discriminator}`)
-			.catch((e) => {
-				console.error(e);
-				return false;
-			});
-	}
+		let validated: Record<string, string> = { wallet: claims.wallet };
+		if (claims.discord) {
+			let { user } = await Validator.discord(claims.discord);
+			validated.discord = `${user.username}#${user.discriminator}`;
+		}
 
-	const tomorrow = Math.floor(Date.now() / 1000 + 24 * 60 * 60);
-	let token = await jwt.sign(
-		{
-			iss: 'supabase',
-			ref: 'lmxcedbadtekgdeasjfn',
-			role: 'anon',
-			exp: tomorrow,
-			// iat:  Math.floor(Date.now() / 1000), // implicitly inserted
-			...validated,
-		},
-		context.env.JWT_SECRET
-	);
+		let tomorrow = Date.now() + 24 * 60 * 60 * 1000;
+		let token = await jwt.sign(
+			{
+				iss: 'supabase',
+				ref: 'lmxcedbadtekgdeasjfn',
+				role: 'anon',
+				exp: Math.floor(tomorrow / 1000),
+				// iat:  Math.floor(Date.now() / 1000), // implicitly inserted
+				...validated,
+			},
+			context.env.JWT_SECRET
+		);
 
-	return new Response(JSON.stringify(validated), {
-		status: 200,
-		headers: {
+		let tmr = new Date(tomorrow).toUTCString();
+
+		let headers = {
 			'content-type': 'application/json;charset=UTF-8',
-			'set-cookie': `__Host-monstrx-token=${token}; Secure; Path=/; Expires=${new Date(
-				tomorrow * 1000
-			).toUTCString()};`,
-		},
-	});
+			'set-cookie': `__Host-monstrx-token=${token}; Secure; Path=/; Expires=${tmr};`,
+		};
+
+		return new Response(JSON.stringify(validated), { status: 200, headers });
+	} catch (e) {
+		// eslint-disable-next-line no-console
+		console.error(e);
+		return new Response(null, { status: 401 });
+	}
 };
