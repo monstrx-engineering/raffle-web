60af1090dd29e2f7c2759c6fe3366368a9926bed
Geraldi Sutanto <5717479+justomat@users.noreply.github.com>
2023-04-27 14:17:40 +0700

    hide venti, fix yugaea

--- pages/pfp/[type].tsx
+++ pages/pfp/[type].tsx
@@ -34,7 +34,7 @@ export default function TypeDetailPage() {
 					)}
 					<Button onClick={generate}>Generate</Button>
 				</Stack>
-				<Card sx={{ flex: 1 }}>
+				<Card sx={{ flex: 1 }} styles={{ root: {} }}>
 					<h3>PFP Generator</h3>
 					<h5>{String(type).toUpperCase()}</h5>
 
@@ -44,39 +44,14 @@ export default function TypeDetailPage() {
 							switch (type) {
 								case 'nusku':
 									return `Nusku has the ability to control the power of fire, using it to create powerful flames, scorching heat, and even summon destructive infernos. He spent his time in the fighting ring at The Eden. He never wanted to hurt anyone, but he loved the adrenaline of being in a fight`;
-							}
-						}.call()}
-							
-						{function () {
-							switch (type) {
 								case 'enki':
 									return `Enki has the ability to control and manipulate water, using it to summon powerful tidal waves, create devastating floods, and even control the weather. It is a graceful and powerful creature, but also possesses a calm and serene nature that makes it a valuable ally to those who respect its power`;
-							}
-						}.call()}
-
-						{function () {
-							switch (type) {
 								case 'venti':
 									return `Venti mastering the winds & storm, a free-spirited entity, enjoy playing songs, whose mischievous nature and sing-song attitude can sometimes irritate others around him with how flippant she is`;
-							}
-						}.call()}
-
-						{function () {
-							switch (type) {
 								case 'hyperion':
 									return `The God of Heavenly Light `;
-							}
-						}.call()}
-
-						{function () {
-							switch (type) {
 								case 'yugaea':
 									return `God of heavenly light & depicted as a handsome young man crowned with the shining aureole of the Sun who watch the earth across the sky everydayYugaea is an earth elemental, he’s the real definition of cheerful and joyful, he brings happiness to other MonstrS and living being on earth, he loves being outside with his best mate Enki and digging in their earth to discover how civilizations flourished or fell.`;
-							}
-						}.call()}
-
-						{function () {
-							switch (type) {
 								case 'noface':
 									return `Spirit represents the power of positivity and kindness, Its body is translucent, and it seems to be made up of pure energy. Spirit has the ability to manipulate the spiritual energies of the world, using them to heal wounds, dispel curses, and summon powerful protective wards. It is a gentle and benevolent creature, but also possesses a fearsome power that can overwhelm those who threaten it or its allies.`;
 							}
--- pages/pfp/index.tsx
+++ pages/pfp/index.tsx
@@ -1,31 +1,31 @@
-import { Card, Container, Image, SimpleGrid, Title } from '@mantine/core';
+import { AspectRatio, Card, Container, Group, Text } from '@mantine/core';
 import Link from 'next/link';
 
-function TypeCard({ name }) {
+function TypeCard({ type }: { type: string }) {
 	return (
-		<Link href={`/pfp/${name}`}>
-			<Card bg={`url('/${name}.png')`} bgp="38% bottom" bgsz="280%" bgr="no-repeat" withBorder>
-				<Card.Section pt="sm">
-					<Title order={6} c="white" sx={{ textTransform: 'uppercase', textAlign: 'center' }}>
-						{name}
-					</Title>
-				</Card.Section>
-			</Card>
+		<Link href={`/pfp/${type}`}>
+			<AspectRatio ratio={200 / 640} mih={600}>
+				<Card bg={`url('/${type}.png')`} bgp="38% bottom" bgsz="280%" bgr="no-repeat" withBorder>
+					<Text c="white" sx={{ textTransform: 'uppercase', alignSelf: 'flex-start' }}>
+						{type}
+					</Text>
+				</Card>
+			</AspectRatio>
 		</Link>
 	);
 }
 
 export default function ProfilePicturePage() {
 	return (
-		<Container py={60} size="xl" h="100%">
-			<SimpleGrid cols={6} h="100%" sx={{ flex: 1 }}>
-				<TypeCard name="hyperion" />
-				<TypeCard name="noface" />
-				<TypeCard name="nusku" />
-				<TypeCard name="enki" />
-				<TypeCard name="venti" />
-				<TypeCard name="yugaea" />
-			</SimpleGrid>
+		<Container py={60} size="xl" h="100%" display="flex">
+			<Group position="apart" grow sx={{ flex: 1 }}>
+				<TypeCard type="hyperion" />
+				<TypeCard type="noface" />
+				<TypeCard type="nusku" />
+				<TypeCard type="enki" />
+				{/* <TypeCard name="venti" /> */}
+				<TypeCard type="yugaea" />
+			</Group>
 		</Container>
 	);
 }
